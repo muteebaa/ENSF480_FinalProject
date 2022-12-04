7e357545fa7c7e55f971a43b67d6af52384fc9b6
muteeba <muteeba.jamal02@gmail.com>
2022-12-04 16:00:03 -0700

    Integrating both codes

--- Seats.java
+++ Seats.java
@@ -2,18 +2,21 @@ import java.util.LinkedList;
 
 public class Seats {
     private int available;
-    private LinkedList<String> seats;
+    private LinkedList<String> seats= new LinkedList<String>();
+    String next = "A";
 
     Seats(int n){
         available = n;
         for (int i = 0; i < n; i++){
             String s = "A";
             if (i!= 0 && i%5 == 0){
+                System.out.println(i);
                 int charValue = s.charAt(0);
-                String next = String.valueOf( (char) (charValue + 1));
-                s = next;
+                next = String.valueOf( (char) (charValue + 1));      
+                System.out.println(s);
             }
-            s += String.valueOf(i);
+            s = next;
+            s += String.valueOf(i+1);
             seats.add(s);
         }
     }
