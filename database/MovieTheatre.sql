c79ec2ff7ab0637848cb32b7ef92f1bf1d80145f
shahzill <shahzill.nvd@gmail.com>
2022-12-05 12:36:48 -0700

    Updated database

--- database/MovieTheatre.sql
+++ database/MovieTheatre.sql
@@ -3,42 +3,42 @@ CREATE DATABASE DATA_CINEMA;
 USE DATA_CINEMA;
 
 DROP TABLE IF EXISTS projectData;
-
 CREATE TABLE projectData (
 	Item		int not null AUTO_INCREMENT,
 	Theatre			varchar(100),
-	Movie		varchar(100),
-	dateM	varchar(25),
+	Movie			varchar(100),
+	dateM			varchar(25),
 	dayM			varchar(25),
 	timeM			varchar(25),
-    Seats	varchar(25),
+    Seats			varchar(25),
+    memberOnly      boolean,
     primary key (Item)
 );
 
-INSERT INTO projectData (Theatre, Movie, dateM, dayM, timeM, Seats)
+INSERT INTO projectData (Theatre, Movie, dateM, dayM, timeM, Seats, memberOnly)
 VALUES
-('Crowfoot Cinema', 'Wakanda Forvever', '23/11/2022', 'Tuesday', '2300', '10'),
-('Crowfoot Cinema', 'Smile', '24/11/2022', 'Tuesday', '2359',  '10'),
-('MarketMall Cinema', 'Black Adam', '25/11/2022', 'Wednesday', '1900',  '10'),
-('Sunridge Cinema', 'Wakanda Forvever', '24/11/2022', 'Wednesday', '1100', '10'),
-('Sunridge Cinema', 'Smile', '24/11/2022', 'Wednesday', '0500', '10'),
-('Crowfoot Cinema', 'Wakanda Forvever', '23/11/2022', 'Wednesday', '0530', '10'),
-('Crowfoot Cinema', 'Smile', '23/11/2022', 'Wednesday', '0545', '10'),
-('MarketMall Cinema', 'Wakanda Forvever', '23/11/2022', 'Wednesday', '0550', '10'),
-('Sunridge Cinema', 'Wakanda Forvever', '23/11/2022', 'Wednesday', '0525', '10'),
-('MarketMall Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0520', '10'),
-('Crowfoot Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0521', '10'),
-('MarketMall Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0522', '10'),
-('Crowfoot Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0534', '10'),
-('Sunridge Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0556', '10'),
-('MarketMall Cinema', 'Wakanda Forvever', '23/11/2022', 'Wednesday', '0517', '10'),
-('Crowfoot Cinema', 'Wakanda Forvever', '23/11/2022', 'Wednesday', '0538', '10'),
-('Sunridge Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0534', '10'),
-('Crowfoot Cinema', 'Smile', '23/11/2022', 'Wednesday', '0542', '10'),
-('MarketMall Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0543', '10'),
-('MarketMall Cinema', 'Wakanda Forvever', '23/11/2022', 'Wednesday', '0541', '10'),
-('Crowfoot Cinema', 'Wakanda Forvever', '23/11/2022', 'Wednesday', '0529', '10'),
-('Crowfoot Cinema', 'Smile', '23/11/2022', 'Wednesday', '0519', '10');
+('Crowfoot Cinema', 'Wakanda Forever', '23/11/2022', 'Tuesday', '2300', '10', 1),
+('Crowfoot Cinema', 'Smile', '24/11/2022', 'Tuesday', '2359',  '10', 0),
+('MarketMall Cinema', 'Black Adam', '25/11/2022', 'Wednesday', '1900',  '10', 0),
+('Sunridge Cinema', 'Wakanda Forever', '24/11/2022', 'Wednesday', '1100', '10', 1),
+('Sunridge Cinema', 'Smile', '24/11/2022', 'Wednesday', '0500', '10', 0),
+('Crowfoot Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0530', '10', 1),
+('Crowfoot Cinema', 'Smile', '23/11/2022', 'Wednesday', '0545', '10', 0),
+('MarketMall Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0550', '10', 1),
+('Sunridge Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0525', '10', 1),
+('MarketMall Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0520', '10', 1),
+('Crowfoot Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0521', '10', 0),
+('MarketMall Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0522', '10', 0),
+('Crowfoot Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0534', '10', 0),
+('Sunridge Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0556', '10', 0),
+('MarketMall Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0517', '10', 1),
+('Crowfoot Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0538', '10', 1),
+('Sunridge Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0534', '10', 0),
+('Crowfoot Cinema', 'Smile', '23/11/2022', 'Wednesday', '0542', '10', 0),
+('MarketMall Cinema', 'Black Adam', '23/11/2022', 'Wednesday', '0543', '10', 0),
+('MarketMall Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0541', '10', 1),
+('Crowfoot Cinema', 'Wakanda Forever', '23/11/2022', 'Wednesday', '0529', '10', 1),
+('Crowfoot Cinema', 'Smile', '23/11/2022', 'Wednesday', '0519', '10', 0);
 
 
 
